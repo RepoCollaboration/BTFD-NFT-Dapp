3ae10537e2e97046a5066a979132b1f0cc5af9bb
RepoCollaboration <156623190+RepoCollaboration@users.noreply.github.com>
2024-01-14 19:03:01 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
 import {
   ThirdwebProvider,
-  embeddedWallet,
-  smartWallet,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  safeWallet,
+  trustWallet,
+  phantomWallet,
 } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/navbar";
@@ -11,20 +16,34 @@ import Header from "../components/header";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const activeChain = "Polygon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
-      supportedWallets={[
-        smartWallet(embeddedWallet(), {
-          factoryAddress: "0x9838b534cd5950CB6ea9E7fa94c00CF3986F953B",
-          gasless: true,
+       supportedWallets={[
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        safeWallet({
+          personalWallets: [
+            metamaskWallet(),
+            coinbaseWallet(),
+            walletConnect(),
+            trustWallet(),
+            phantomWallet(),
+          ],
         }),
+        trustWallet(),
+        phantomWallet(),
       ]}
     >
+      <ConnectWallet
+        theme={"dark"}
+        modalSize={"wide"}
+      />
       <Header />
       <Component {...pageProps} />
       <Navbar />
