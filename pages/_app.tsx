c1c1a0f0ef5479cf959d6bb37af96840b0785bf6
RepoCollaboration <156623190+RepoCollaboration@users.noreply.github.com>
2024-01-14 19:07:17 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ import Header from "../components/header";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "Polygon";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
